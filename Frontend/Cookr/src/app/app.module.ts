5d782881b42294f249bdfeb91c0e1d14698cc45c
Beelzseals <88729970+Beelzseals@users.noreply.github.com>
2022-11-01 14:05:48 +0100

    version 0.1

--- Frontend/Cookr/src/app/app.module.ts
+++ Frontend/Cookr/src/app/app.module.ts
@@ -7,12 +7,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NavbarComponent } from './navbar/navbar.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 
 @NgModule({
